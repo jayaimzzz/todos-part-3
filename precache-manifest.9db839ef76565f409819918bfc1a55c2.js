f69db0be903cdc5a4d16c9638162732a06ab3a63
jayaimzzz <jameshorton@gmail.com>
2018-11-20 13:55:43 -0500

    Updates

---
+++ precache-manifest.9db839ef76565f409819918bfc1a55c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "938174d8dd7868e619ab",
+    "url": "/todos-part-3/static/css/main.91631b2c.chunk.css"
+  },
+  {
+    "revision": "938174d8dd7868e619ab",
+    "url": "/todos-part-3/static/js/main.938174d8.chunk.js"
+  },
+  {
+    "revision": "b984fc1eeeb147d15895",
+    "url": "/todos-part-3/static/js/1.b984fc1e.chunk.js"
+  },
+  {
+    "revision": "eb5d57de38284f0a3d03",
+    "url": "/todos-part-3/static/js/runtime~main.eb5d57de.js"
+  },
+  {
+    "revision": "ebf7c9c736ff181697025d054ba18c6c",
+    "url": "/todos-part-3/index.html"
+  }
+];
